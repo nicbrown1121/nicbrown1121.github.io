4e55fbc30a5caf4150c2d500e2b77a5f723a3332
Nicole Brown <nicolebrown@Nicoles-MacBook-Air.local>
2023-06-26 16:59:08 -0400

    removeed icons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-import { IoNewspaperSharp } from "react-icons/io5";
+import React from "react";
 import { AiFillLinkedin } from "react-icons/ai";
-import { RiMentalHealthFill } from "react-icons/ri";
 import Card from "./Card";
 
 const Home = () => {
